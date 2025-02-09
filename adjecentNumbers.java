c38348c6536f84326b7709d8308048f9445871da
chaitanyashrivastav <101629454+chaitanyashrivastav@users.noreply.github.com>
2025-02-09 09:22:01 +0530

    Add files via upload

---
+++ adjecentNumbers.java
@@ -0,0 +1,13 @@
+class adjecentNumbers{
+	public static void main(String[] args){
+		int[] arr={4,8,6,3,4,5,4};
+		//int n=arr.length;
+		for(int i=0;i<arr.length-1;i++){
+			if(arr[i]==3){
+			System.out.println(i);
+			}
+		}
+		
+		
+	}
+}
