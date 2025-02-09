254bb23628689cb4b9002ddb4cdaeb6ea89b819a
chaitanyashrivastav <101629454+chaitanyashrivastav@users.noreply.github.com>
2025-02-09 09:22:51 +0530

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Anagram{
+	public static void checkAn(String str1,String str2){
+		char[] arr1=str1.toCharArray();
+		char[] arr2=str2.toCharArray();
+		
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		if(Arrays.equals(arr1,arr2)){
+			System.out.println("yes it is an anagram");
+		}
+		else{
+			System.out.println("no not a anagram");
+		}
+	}
+	
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	String str1=sc.nextLine().toLowerCase();
+	String str2=sc.nextLine().toLowerCase();
+	checkAn(str1,str2);
+}
+}
